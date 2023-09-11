c56f4cd029d3e05c55f3dae0c01fc8e198969faf
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-09-12 02:39:53 +0530

    Commented out the two lines

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
 
-        obj.name = "New_name"; //error: 'name' has private access in 'com.driver.RWOnly'
-        System.out.println(obj.name); //error: 'name' has private access in 'com.driver.RWOnly'
+//        obj.name = "New_name"; //error: 'name' has private access in 'com.driver.RWOnly'
+//        System.out.println(obj.name); //error: 'name' has private access in 'com.driver.RWOnly'
 
         obj.setName("New_name");
         System.out.println(obj.getName());
