a662b51a1bb7737340d1a21e035e2b0658aeb12b
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-09-12 02:36:45 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        obj.name = "New_name"; //error: 'name' has private access in 'com.driver.RWOnly'
+        System.out.println(obj.name); //error: 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("New_name");
+        System.out.println(obj.getName());
+    }
 }
